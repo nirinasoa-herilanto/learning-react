c9c61539ab48ff6ad387598aba55268e83bd927e
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-03-17 15:23:55 +0300

    styles: dynamic styles in css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
+import styles from './CourseInput.module.css';
 import Button from '../../UI/Button/Button';
 
 const CourseInput = (props) => {
@@ -28,47 +27,15 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
-
-const FormControl = styled.div`
-  & {
-    margin: 0.5rem 0;
-  }
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props?.invalid ? 'red' : 'black')};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props?.invalid ? 'red' : '#ccc')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    background: ${(props) => (props?.invalid ? ' #ffd7d7' : 'transparent')};
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: #ffd7d7;
-  }
-`;
 
 export default CourseInput;
