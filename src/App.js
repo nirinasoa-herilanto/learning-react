43c4540c68a9d5c5c8cf9f76fab55edcb412af30
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-04-04 09:23:39 +0300

    project starter code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import './App.css';
-import React from 'react';
-
 function App() {
-  React.useEffect(() => {
-    document.title = 'Welcome to the app';
-  }, []);
-
   return (
-    <div className="app">
-      <div className="app__content">
-        <h1 className="title">Welcome to my project</h1>
-        <p>
-          An application about learning and deep dive into React.JS framework
-        </p>
-      </div>
+    <div>
+      <h2>Let's get started!</h2>
     </div>
   );
 }
