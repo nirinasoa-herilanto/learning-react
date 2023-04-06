58e639bf7affafdaaaea5e3cea345aec2d62849b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-04-06 10:44:04 +0300

    update: adding cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const initialCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemToCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const items = state.items.concat(action.item);
+    const totalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return { items, totalAmount };
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+  }
+
+  return initialCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    initialCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_ITEM', item });
+  };
+
+  const removeItemToCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
