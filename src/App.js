97c829225d69f77d4c918201ccd5f125f0c04b8f
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-04-11 10:44:42 +0300

    feat: a closer look at child component re-evaluation
    re-evaluating components !== re-rendering the DOM

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -16,7 +17,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is a paragraphs</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle paragraphs</Button>
     </div>
   );
---
+++ src/components/UI/Button/Demo/DemoOutput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import MyParagraph from './MyParagraph';
+
+const DemoOutput = (props) => {
+  console.log('DemoOutput Running');
+  return <MyParagraph>{props.show ? 'This is a paragraphs' : ''}</MyParagraph>;
+};
+
+export default DemoOutput;
