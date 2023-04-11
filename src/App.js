ca3e115fd9690f43ab5d02780c7835bd61a9a55a
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-04-11 10:19:38 +0300

    feat: component update in actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
+import Button from './components/UI/Button/Button';
+
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('App running');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prev) => !prev);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is a paragraphs</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle paragraphs</Button>
     </div>
   );
 }
