f8dde155e2c974990e4ac00503386816ec3451c1
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-18 12:12:25 +0300

    refactor: updating common component (Header)

--- src/components/Concepts/Concepts.js
+++ src/components/Concepts/Concepts.js
@@ -1,11 +1,12 @@
-import ConceptItem from './ConceptItem';
 import './Concepts.css';
 import React from 'react';
+
+import ConceptItem from './ConceptItem';
 
 const Concepts = (props) => {
   return (
     <ul id="concepts">
-      {props.data.map((item) => (
+      {props.data?.map((item) => (
         <ConceptItem
           key={item.title}
           imageUrl={item.image}
