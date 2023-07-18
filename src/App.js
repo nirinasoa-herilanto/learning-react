bdf83e6d613bd55910cf080540c0088fe945d57a
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-18 11:31:46 +0300

    practice starter code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import './App.css';
-import React from 'react';
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
 
-function App() {
-  React.useEffect(() => {
-    document.title = 'Welcome to the app';
-  }, []);
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
 
+const App = () => {
   return (
-    <div className="app">
-      <div className="app__content">
-        <h1 className="title">Welcome to my project</h1>
-        <p>
-          An application about learning and deep dive into React.JS framework
-        </p>
-      </div>
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        <li className="concept">
+          <img src="TODO: IMAGE" alt="TODO: TITLE" />
+          <h2>TODO: TITLE</h2>
+          <p>TODO: DESCRIPTION</p>
+        </li>
+      </ul>
     </div>
   );
 }
