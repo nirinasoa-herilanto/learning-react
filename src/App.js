edaaa61581118799c5b18522a72704efcd10538c
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-26 18:17:16 +0300

    update & refactor

--- src/App.js
+++ src/App.js
@@ -1,98 +1,39 @@
-import logo from './assets/investment-calculator-logo.png';
+import React, { useState } from 'react';
 
-function App() {
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+import { calculateHandler } from './utils/calculateHandler';
 
-    const yearlyData = []; // per-year results
+import Header from './components/Header/Header';
+import CalculateForm from './components/CalculateForm/CalculateForm';
+import ResultsTable from './components/ResultsTable/ResultsTable';
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+const App = () => {
+  const [data, setData] = useState(null);
+  const [initialInvestment, setInitialInvestment] = useState(0);
 
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
+  const calculateInvestmentHandler = (userInput) => {
+    if (!userInput) {
+      setData(null);
+      return;
     }
 
-    // do something with yearlyData ...
+    setInitialInvestment(userInput['current-savings']);
+
+    const results = calculateHandler(userInput);
+    setData(results);
   };
 
   return (
     <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-      </header>
+      <Header />
+      <CalculateForm onCalculateInvestment={calculateInvestmentHandler} />
 
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
-
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      {!data ? (
+        <div className="no-investment">No investment calculation yet.</div>
+      ) : (
+        <ResultsTable data={data} initialInvestment={initialInvestment} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
