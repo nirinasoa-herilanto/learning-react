c510345e79f671be503248c0e59f819fa7ded21f
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-03 10:20:37 +0300

    feat: build first custom hook with useCounter

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useCounter = ({ operation = 'increment' | 'decrement', value = 1 }) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (operation === 'increment') {
+        setCounter((prevCounter) => prevCounter + value);
+      } else {
+        setCounter((prevCounter) => prevCounter - value);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [operation]);
+
+  return counter;
+};
+
+export default useCounter;
