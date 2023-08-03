53a6a7034106c7c14ed2454c510d10c202708a39
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-03 09:49:42 +0300

    project starter code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
 import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  React.useEffect(() => {
-    document.title = 'Welcome to the app';
-  }, []);
-
   return (
-    <div className="app">
-      <div className="app__content">
-        <h1 className="title">Welcome to my project</h1>
-        <p>
-          An application about learning and deep dive into React.JS framework
-        </p>
-      </div>
-    </div>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 
