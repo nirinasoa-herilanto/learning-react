8df8c9a5da2ff0afdb4523097a4ffe950c0c00ae
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-04 15:46:24 +0300

    feat: dealing with form submissions

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,36 @@
+import React, { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    const enteredNameValue = nameInputRef.current.value;
+    console.log(enteredName);
+    console.log(enteredNameValue);
+
+    // nameInputRef.current.value = ''; // Not Ideal, don't manipulate the DOM directly
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          ref={nameInputRef}
+          id="name"
+          value={enteredName}
+          onChange={nameChangeHandler}
+        />
       </div>
+
       <div className="form-actions">
         <button>Submit</button>
       </div>
