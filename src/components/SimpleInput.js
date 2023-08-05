b686e8caf90eddf264bb249a6b53f1c3506eed2b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-05 13:46:33 +0300

    update: managing the overall form validity
    - by using the concepts of deriving state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
@@ -48,7 +54,7 @@ const SimpleInput = (props) => {
       </div>
 
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
