f9b6487cad24935cb4c68d2d97c00e373fef7cf3
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-09 13:48:07 +0300

    updates markup

--- src/App.js
+++ src/App.js
@@ -9,16 +9,24 @@ import Cart from './components/Cart/Cart';
 
 const App = () => {
   const [cartIsShow, setCartIsShow] = useState(false);
+  const [showOrderMeals, setShowOrderMeals] = useState(false);
 
   const showCartHandler = () => setCartIsShow(true);
   const hideCartHandler = () => setCartIsShow(false);
+  const toggleOrderMealsHandler = () => setShowOrderMeals((prev) => !prev);
 
   return (
     <AuthProvider>
       <CartProvider>
         <Header onShowCart={showCartHandler} />
 
-        {cartIsShow && <Cart onHideCart={hideCartHandler} />}
+        {cartIsShow && (
+          <Cart
+            showOrderMeals={showOrderMeals}
+            onToggleOrderMeals={toggleOrderMealsHandler}
+            onHideCart={hideCartHandler}
+          />
+        )}
 
         <main>
           <Meals />
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,25 +3,15 @@ import styles from './Header.module.css';
 
 import mealsImage from '../../assets/meals.jpg';
 
-import { useAuthStore } from '../../store/auth/auth-context';
-
 import HeaderCartButton from './HeaderCartButton';
-import Button from '../UI/Button';
 
 const Header = (props) => {
-  const { user, signingWithGoogleHandler, loggoutHandler } = useAuthStore();
-
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>React Meals</h1>
 
-        <div>
-          <HeaderCartButton onClick={props.onShowCart} />
-
-          {!user && <Button onClick={signingWithGoogleHandler}>Sign in</Button>}
-          {user && <Button onClick={loggoutHandler}>Log out</Button>}
-        </div>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
 
       <div className={styles['main-image']}>
