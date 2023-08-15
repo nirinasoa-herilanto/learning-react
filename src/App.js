e2b58fe1430a959e365b5d858b22dfb14c103562
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-15 09:44:22 +0300

    project starte code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
-import React from 'react';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  React.useEffect(() => {
-    document.title = 'Welcome to the app';
-  }, []);
-
   return (
-    <div className="app">
-      <div className="app__content">
-        <h1 className="title">Welcome to my project</h1>
-        <p>
-          An application about learning and deep dive into React.JS framework
-        </p>
-      </div>
-    </div>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
