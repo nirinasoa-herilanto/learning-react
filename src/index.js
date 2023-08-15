6a477dac5d5c5fda8faaafa39815ffa9c491a16b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-15 11:08:05 +0300

    update: add Provider wrapper

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import store from './store/index';
+
+import App from './App';
+import { Provider as ReduxProvider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
 
