eca8459d9e0590dc0c50f73ecb1e3095b71cbe3e
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-16 10:12:31 +0300

    update: useEffect & redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -8,6 +9,15 @@ function App() {
   const showShoppinCart = useSelector(
     (state) => state.shop.displayShoppingCart
   );
+  const shoppingCart = useSelector((state) => state.shop.shoppingCart);
+
+  useEffect(() => {
+    shoppingCart?.length !== 0 &&
+      window.localStorage.setItem(
+        'shopping-cart',
+        JSON.stringify(shoppingCart)
+      );
+  }, [shoppingCart]);
 
   return (
     <Layout>
