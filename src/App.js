5223135489caef91dcf4a723034dbf817236b90d
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-17 10:12:59 +0300

    update: adding new routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/products',
+    element: <ProductsPage />,
   },
 ]);
 
