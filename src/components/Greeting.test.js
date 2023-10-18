7741261548066e9ff5759c41b468b061edd7d09e
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-10-18 11:14:51 +0300

    feat: Grouping Tests Together With Test Suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('Renders Hello world text', () => {
-  // Arranage
-  render(<Greeting />);
+describe('Greeting component', () => {
+  test('Renders Hello world text', () => {
+    // Arranage
+    render(<Greeting />);
 
-  // Act, nothing
+    // Act, nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText('Hello world', { exact: false }); // by default exact is true
+    // Assert
+    const helloWorldElement = screen.getByText('Hello world', { exact: false }); // by default exact is true
 
-  expect(helloWorldElement).toBeInTheDocument();
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
