85de53b0631eb24d4b373501aafcc108a5f27d52
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-10-18 10:52:19 +0300

    feat: running first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Welcome to my project title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const welcomeTitleElement = screen.getByText(/Welcome to my project/i);
+
+  expect(welcomeTitleElement).toBeInTheDocument();
 });
