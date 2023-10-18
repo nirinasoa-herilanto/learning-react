c16d26b26f97b0f5c9e16d34a3871c77acbb7815
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-10-18 13:22:45 +0300

    feat: working with mock

--- src/components/Posts.test.js
+++ src/components/Posts.test.js
@@ -4,6 +4,11 @@ import Posts from './Posts';
 
 describe('Posts component', () => {
   test('renders posts data', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p-1', title: 'first post' }],
+    });
+
     render(<Posts />);
 
     const listItemMarkups = await screen.findAllByRole('list');
